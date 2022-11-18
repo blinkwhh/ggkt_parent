a954d9e4a597d9cce8837a5eab2a6cc685e39227
e <1730121019@qq.com>
2022-11-19 00:10:20 +0800

    add model gateway

---
+++ service_gateway/src/main/java/com/atguigu/ggkt/gateway/ApiGatewayApplication.java
@@ -0,0 +1,16 @@
+package com.atguigu.ggkt.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 王寒寒
+ * @create 2022-11-19 0:04
+ */
+
+@SpringBootApplication
+public class ApiGatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayApplication.class, args);
+    }
+}
