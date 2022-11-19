883c0a71550831ab0d63117ace5d95052919753b
e <1730121019@qq.com>
2022-11-19 23:53:19 +0800

    add service_client

---
+++ service_client/service_user_client/src/main/java/UserInfoFeignClient.java
@@ -0,0 +1,17 @@
+import com.atguigu.ggkt.model.user.UserInfo;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+/**
+ * @author 王寒寒
+ * @create 2022-11-19 23:49
+ */
+@FeignClient(value = "service-user")
+public interface UserInfoFeignClient {
+
+    @GetMapping("/admin/user/userInfo/inner/getById/{id}")
+    public UserInfo getById(@PathVariable Long id);
+
+
+}
