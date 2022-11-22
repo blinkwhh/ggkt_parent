922c8704bc781a20e8b64fdf9cfba84310a2e99d
e <1730121019@qq.com>
2022-11-22 23:40:46 +0800

    add verifyToken method in Controller

---
+++ service/service_wechat/src/main/java/com/atguigu/ggkt/wechat/controller/MessageController.java
@@ -0,0 +1,61 @@
+package com.atguigu.ggkt.wechat.controller;
+
+import com.atguigu.ggkt.wechat.utils.SHA1;
+import lombok.extern.java.Log;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Arrays;
+
+/**
+ * @author 王寒寒
+ * @create 2022-11-22 23:13
+ */
+@RestController
+@RequestMapping("/api/wechat/message")
+@Slf4j
+public class MessageController {
+
+//    TODO
+    private static final String token = "ggkt";
+
+
+    /**
+     * 服务器有效性验证
+     * @param request
+     * @return
+     */
+    @GetMapping
+    public String verifyToken(HttpServletRequest request) {
+        String signature = request.getParameter("signature");
+        String timestamp = request.getParameter("timestamp");
+        String nonce = request.getParameter("nonce");
+        String echostr = request.getParameter("echostr");
+        log.info("signature: {} nonce: {} echostr: {} timestamp: {}", signature, nonce, echostr, timestamp);
+        if (this.checkSignature(signature, timestamp, nonce)) {
+            log.info("token ok");
+            return echostr;
+        }
+        return echostr;
+    }
+
+    private boolean checkSignature(String signature, String timestamp, String nonce) {
+        String[] str = new String[]{token, timestamp, nonce};
+        //排序
+        Arrays.sort(str);
+        //拼接字符串
+        StringBuffer buffer = new StringBuffer();
+        for (int i = 0; i < str.length; i++) {
+            buffer.append(str[i]);
+        }
+        //进行sha1加密
+        String temp = SHA1.encode(buffer.toString());
+        //与微信提供的signature进行匹对
+        return signature.equals(temp);
+    }
+
+
+}
