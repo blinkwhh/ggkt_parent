7bc1dad67029f8c36842a6cba5d73aee25d5b3ae
e <1730121019@qq.com>
2022-11-25 11:17:15 +0800

    wechat authorize login interface

---
+++ service/service_user/src/main/java/com/atguigu/ggkt/user/utils/ConstantPropertiesUtil.java
@@ -0,0 +1,28 @@
+package com.atguigu.ggkt.user.utils;
+
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author 王寒寒
+ * @create 2022-11-25 11:04
+ */
+@Component
+public class ConstantPropertiesUtil implements InitializingBean {
+
+    @Value("${wechat.mpAppId}")
+    private String appid;
+
+    @Value("${wechat.mpAppSecret}")
+    private String appsecret;
+
+    public static String ACCESS_KEY_ID;
+    public static String ACCESS_KEY_SECRET;
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        ACCESS_KEY_ID = appid;
+        ACCESS_KEY_SECRET = appsecret;
+    }
+}
---
+++ service/service_user/src/main/java/com/atguigu/ggkt/user/utils/WeChatMpConfig.java
@@ -0,0 +1,35 @@
+package com.atguigu.ggkt.user.utils;
+
+import me.chanjar.weixin.mp.api.WxMpConfigStorage;
+import me.chanjar.weixin.mp.api.WxMpInMemoryConfigStorage;
+import me.chanjar.weixin.mp.api.WxMpService;
+import me.chanjar.weixin.mp.api.impl.WxMpServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author 王寒寒
+ * @create 2022-11-25 11:04
+ */
+@Component
+public class WeChatMpConfig {
+
+    @Autowired
+    private ConstantPropertiesUtil constantPropertiesUtil;
+
+    @Bean
+    public WxMpService wxMpService(){
+        WxMpService wxMpService = new WxMpServiceImpl();
+        wxMpService.setWxMpConfigStorage(wxMpConfigStorage());
+        return wxMpService;
+    }
+
+    @Bean
+    public WxMpConfigStorage wxMpConfigStorage(){
+        WxMpInMemoryConfigStorage wxMpConfigStorage = new WxMpInMemoryConfigStorage();
+        wxMpConfigStorage.setAppId(ConstantPropertiesUtil.ACCESS_KEY_ID);
+        wxMpConfigStorage.setSecret(ConstantPropertiesUtil.ACCESS_KEY_SECRET);
+        return wxMpConfigStorage;
+    }
+}
