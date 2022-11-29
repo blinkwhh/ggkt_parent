907264d2d9e07dd7568fa0f457f2bfac775d377e
e <1730121019@qq.com>
2022-11-30 00:22:02 +0800

    search livecourse interface(completed)

--- service/service_live/src/main/java/com/atguigu/ggkt/live/service/impl/LiveCourseServiceImpl.java
+++ service/service_live/src/main/java/com/atguigu/ggkt/live/service/impl/LiveCourseServiceImpl.java
@@ -1,13 +1,18 @@
 package com.atguigu.ggkt.live.service.impl;
 
 
+import com.atguigu.ggkt.client.course.CourseFeignClient;
 import com.atguigu.ggkt.live.mapper.LiveCourseMapper;
 import com.atguigu.ggkt.live.service.LiveCourseService;
 import com.atguigu.ggkt.model.live.LiveCourse;
+import com.atguigu.ggkt.model.vod.Teacher;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * <p>
@@ -20,9 +25,20 @@ import org.springframework.stereotype.Service;
 @Service
 public class LiveCourseServiceImpl extends ServiceImpl<LiveCourseMapper, LiveCourse> implements LiveCourseService {
 
+    @Autowired
+    private CourseFeignClient courseFeignClient;
     //直播课程分页查询
     @Override
     public IPage<LiveCourse> selectPage(Page<LiveCourse> pageParam) {
-        return null;
+        IPage<LiveCourse> page = baseMapper.selectPage(pageParam, null);
+        List<LiveCourse> liveCourseList = page.getRecords();
+
+        for(LiveCourse liveCourse : liveCourseList) {
+            Teacher teacher = courseFeignClient.getTeacherLive(liveCourse.getTeacherId());
+            //这里判断查到的是否为空
+            liveCourse.getParam().put("teacherName", teacher.getName());
+            liveCourse.getParam().put("teacherLevel", teacher.getLevel());
+        }
+        return page;
     }
 }
